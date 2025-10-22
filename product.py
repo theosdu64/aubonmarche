7270e928c004478477872f472b5ee94c88bb237f
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-22 15:06:24 +0200

    add str and repr func

--- product.py
+++ product.py
@@ -6,4 +6,9 @@ class Product:
         self.unite = unite
         self.category = category
 
-    
+    def __repr__(self):
+        return (f"name={self.name}, prix={self.prix}€, stock={self.stock} {self.unite}, "
+                f"category={self.category})")
+
+    def __str__(self):
+        return (f"{self.name} - {self.prix}€ - {self.stock} {self.unite} - catégorie : {self.category}")
