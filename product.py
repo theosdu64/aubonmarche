34676f4e39ff97a5a4b891f059971880306d0f42
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-22 14:19:00 +0200

    init product class

---
+++ product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, name, prix, stock, unite, category):
+        self.name = name
+        self.prix = prix
+        self.stock = stock
+        self.unite = unite
+        self.category = category
+
+    
