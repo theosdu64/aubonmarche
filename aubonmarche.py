69a1dca87bc212fed1f2b799b428f1b094b4c90d
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-22 15:04:39 +0200

    aubonmarche file corection delete class

--- aubonmarche.py
+++ aubonmarche.py
@@ -1,8 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-class Client:
-	def __init__(self, firstname : str, lastname: str) :
-		self.firstname = firstname
-		self.lastname = lastname
-		self.cart = []
