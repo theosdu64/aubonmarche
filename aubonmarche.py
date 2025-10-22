6bb205e7e15813487d834c77254f973c5e7fa017
Yassine <yassine.zriba@fms-ea.com>
2025-10-22 14:31:05 +0200

    Init class Client

--- aubonmarche.py
+++ aubonmarche.py
@@ -1,1 +1,8 @@
-print("salut")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Client:
+	def __init__(self, firstname : str, lastname: str) :
+		self.firstname = firstname
+		self.lastname = lastname
+		self.cart = []
