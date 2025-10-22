cca533eea0098e76b011f017603ebd87e47757ca
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-22 15:07:13 +0200

    reformate shop init varand instanciation de prooduct

--- shop.py
+++ shop.py
@@ -1,8 +1,25 @@
 from data import produits_data
+from product import Product
 
 class Shop:
     def __init__(self):
-        self.stock = produits_data
+        self.stock = {}
+        for name, info in produits_data.items():
+            self.stock[name] = Product(
+                name=name,
+                prix=info["prix"],
+                stock=info["stock"],
+                unite=info['unite'],
+                category=info['categorie']
+            )
 
 store = Shop()
-print(store.stock)
+poire = store.stock['Poire']
+print(poire.name)     
+print(poire.prix)      
+print(poire.stock)     
+print(poire.unite)     
+print(poire.category)  
+
+print('-' * 60)
+print(store.stock['Poire'])
