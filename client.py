e1a4a9a5654fa82fe0b86e963e452442d858670c
Yassine <yassine.zriba@fms-ea.com>
2025-10-22 14:37:15 +0200

    Modification in to client fil

---
+++ client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Client:
+	def __init__(self, firstname : str, lastname: str) :
+		self.firstname = firstname
+		self.lastname = lastname
+		self.cart = []
