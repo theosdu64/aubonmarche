63592810db4cfa3a2e396a0bdb71c467fc3f1e88
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-23 11:52:01 +0200

    create format array func to format list of product

--- aubonmarche.py
+++ aubonmarche.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from client import Client
+from shop import Shop 
+
+shop = Shop()
 
 def nav_question():
     result = input("Bilan ou Achetez ou Stoppez(1 ou 2 ou 3)" )
@@ -18,6 +21,15 @@ def ask_name():
 def product_category():
     category = input('quel category de produit ? (1 : Legume, 2: Fruit)')
     print(category)
+    return int(category)
+
+def format_array(list_product):
+    format_dict = {}
+    for index, product in enumerate(list_product):
+        format_dict[index + 1] = product  
+    return format_dict
+
+# def choose_product 
 
 def main():
     continu = True
@@ -26,6 +38,21 @@ def main():
         print(numNav)
         if numNav == 2:
             ask_name()
-            product_category()
-
+            continu_buy_product = True
+            while continu_buy_product:
+                test = shop.get_products_by_category(product_category())
+                format_array_product = format_array(test)
+                print(format_array_product)
+            # 'poid'
+            # 'compare'
+            # 'append dans client.order'
+            # 'si fini'
+            # ----------------------------------------
+            # 'on recupere client.order', Client
+            # 'addition de client.order' , Cleint
+            # cree la classe ticket : , Ticket
+            # classe Clent cree un list Ticket instancie, Ticket
+            # 'creer un ticket + total + nom, prenom' , Client
+            # 'bilan.append(ticket)'
+            # 'retour dans nav_question'
 main()
